4c59e8d0f07e8bd5af8735fbb8da6333c30b1d74
David <davidlglozo@gmail.com>
2015-11-10 13:31:32 +0100

    Add bootanimation and other things

--- config/common.mk
+++ config/common.mk
@@ -1,37 +1,8 @@
 PRODUCT_BRAND ?= cyanogenmod
 
-ifneq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
-# determine the smaller dimension
-TARGET_BOOTANIMATION_SIZE := $(shell \
-  if [ $(TARGET_SCREEN_WIDTH) -lt $(TARGET_SCREEN_HEIGHT) ]; then \
-    echo $(TARGET_SCREEN_WIDTH); \
-  else \
-    echo $(TARGET_SCREEN_HEIGHT); \
-  fi )
-
-# get a sorted list of the sizes
-bootanimation_sizes := $(subst .zip,, $(shell ls vendor/cm/prebuilt/common/bootanimation))
-bootanimation_sizes := $(shell echo -e $(subst $(space),'\n',$(bootanimation_sizes)) | sort -rn)
-
-# find the appropriate size and set
-define check_and_set_bootanimation
-$(eval TARGET_BOOTANIMATION_NAME := $(shell \
-  if [ -z "$(TARGET_BOOTANIMATION_NAME)" ]; then
-    if [ $(1) -le $(TARGET_BOOTANIMATION_SIZE) ]; then \
-      echo $(1); \
-      exit 0; \
-    fi;
-  fi;
-  echo $(TARGET_BOOTANIMATION_NAME); ))
-endef
-$(foreach size,$(bootanimation_sizes), $(call check_and_set_bootanimation,$(size)))
-
-ifeq ($(TARGET_BOOTANIMATION_HALF_RES),true)
-PRODUCT_BOOTANIMATION := vendor/cm/prebuilt/common/bootanimation/halfres/$(TARGET_BOOTANIMATION_NAME).zip
-else
-PRODUCT_BOOTANIMATION := vendor/cm/prebuilt/common/bootanimation/$(TARGET_BOOTANIMATION_NAME).zip
-endif
-endif
+# Boot animation include
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/common/bootanimation/1080.zip:system/media/bootanimation.zip
 
 ifdef CM_NIGHTLY
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -290,15 +261,31 @@ else
     CM_VERSION := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date -u +%Y%m%d)-$(CM_BUILDTYPE)$(CM_EXTRAVERSION)-$(CM_BUILD)
 endif
 
+# NuclearVersion
+ROM_VERSION = 6.0
+ROM_VERSION_STATUS = alpha
+ROM_VERSION_MAINTENANCE = 1.5
+ROM_POSTFIX := $(shell date +"%Y%m%d-%H%M")
+
+NUCLEAR_VERSION := NucleaROM-V$(ROM_VERSION_MAINTENANCE)-$(ROM_VERSION_STATUS)[$(ROM_VERSION)]-$(ROM_POSTFIX)
+NUCLEAR_MOD_VERSION := NucleaROM-V$(ROM_VERSION_MAINTENANCE)-$(ROM_VERSION_STATUS)[$(ROM_VERSION)]-$(ROM_POSTFIX)
+
 PRODUCT_PROPERTY_OVERRIDES += \
-  ro.cm.version=$(CM_VERSION) \
-  ro.cm.releasetype=$(CM_BUILDTYPE) \
-  ro.modversion=$(CM_VERSION) \
+  ro.cm.version=$(NUCLEAR_VERSION) \
+  ro.cm.releasetype=$(ROM_VERSION_STATUS) \
+  ro.modversion=$(NUCLEAR_VERSION) \
   ro.cmlegal.url=https://cyngn.com/legal/privacy-policy
+
+#PRODUCT_PROPERTY_OVERRIDES += \
+#  ro.cm.version=$(CM_VERSION) \
+#  ro.cm.releasetype=$(CM_BUILDTYPE) \
+#  ro.modversion=$(CM_VERSION) \
+#  ro.cmlegal.url=https://cyngn.com/legal/privacy-policy
 
 -include vendor/cm-priv/keys/keys.mk
 
-CM_DISPLAY_VERSION := $(CM_VERSION)
+#CM_DISPLAY_VERSION := $(CM_VERSION)
+CM_DISPLAY_VERSION := $(NUCLEAR_VERSION)
 
 ifneq ($(PRODUCT_DEFAULT_DEV_CERTIFICATE),)
 ifneq ($(PRODUCT_DEFAULT_DEV_CERTIFICATE),build/target/product/security/testkey)
@@ -355,3 +342,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 -include vendor/cyngn/product.mk
 
 $(call prepend-product-if-exists, vendor/extra/product.mk)
+
+FINISHER_SCRIPT := vendor/cm/tools/finisher
+SQUISHER_SCRIPT := vendor/cm/tools/squisher
